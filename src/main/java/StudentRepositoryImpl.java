8590134e3af56ecadd84a5c0e8ca51e77ac502d9
Roaura <mbox28803@naver.com>
2024-12-31 10:32:11 +0900

    코드 변경

--- src/main/java/StudentRepositoryImpl.java
+++ src/main/java/StudentRepositoryImpl.java
@@ -1,28 +1,56 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRepositoryImpl implements StudentRepository {
+	
+	private final List<Student> students = new ArrayList<>();
+	private int nextId = 1;
+	
     @Override
     public List<Student> findAll() {
-        return null;
+        return new ArrayList<>(students); // students로 리턴 시키면 원본을 건들수도있어서 List를 만들어준거
     }
 
+//    public Student findById(int id) {
+//    	for(int i = 0; i < students.size(); i++) {
+//    		if(students.get(i).getId() == id) {
+//    			return students.get(i);
+//    		}
+//    	} return null;
+//    }
+    
     @Override
     public Student findById(int id) {
-        return null;
+    	for (Student student : students) {
+    		if(student.getId() == id) {
+    			return student;
+    		}
+    	}
+    	return null;
     }
+
 
     @Override
     public void save(Student student) {
-
+    	student = new Student(nextId++, student.getName(), student.getMajor());
+    	students.add(student);
     }
 
     @Override
     public void update(int id, Student updatedStudent) {
-
+    	Student student = findById(id);
+    	if(student != null) {
+    		student.setName(updatedStudent.getName());
+    		student.setMajor(updatedStudent.getMajor());
+    	}
     }
-
     @Override
     public void delete(int id) {
-
-    }
+    	for(int i = 0; i < students.size(); i++) {
+		if(students.get(i).getId() == id) {
+			students.remove(i);
+			break;
+		}
+     }
+   }
 }
